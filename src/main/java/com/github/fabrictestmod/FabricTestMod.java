5a0919130ce702dce5b953ed3d60a8573b40e9a9
Alex <Alex.Porter@ucf.edu>
2021-03-01 09:52:56 -0500

    Added CreeperSoulEssence.java Item

--- src/main/java/com/github/fabrictestmod/FabricTestMod.java
+++ src/main/java/com/github/fabrictestmod/FabricTestMod.java
@@ -1,6 +1,7 @@
 package com.github.fabrictestmod;
 
 import com.github.fabrictestmod.items.CreeperSoulItem;
+import com.github.fabrictestmod.items.CreeperSoulEssence;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -15,12 +16,15 @@ public class FabricTestMod implements ModInitializer {
 	//Define Mod Items
 	public static final Item CREEPER_SOUL = new CreeperSoulItem(
 			new Item.Settings().group(ItemGroup.MATERIALS).maxCount(1).maxDamage(10));
+	public static final Item CREEPER_SOUL_ESSENCE = new CreeperSoulEssence(
+			new Item.Settings().group(ItemGroup.MATERIALS).maxCount(64));
 
 	@Override
 	public void onInitialize() {
 
 		//Register Mod Items
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "creeper_soul"), CREEPER_SOUL);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "creeper_soul_essence"), CREEPER_SOUL_ESSENCE);
 
 		System.out.println("FTM has been initialized");
 	}
---
+++ src/main/java/com/github/fabrictestmod/items/CreeperSoulEssence.java
@@ -0,0 +1,11 @@
+package com.github.fabrictestmod.items;
+
+import net.minecraft.item.Item;
+
+public class CreeperSoulEssence extends Item {
+
+    public CreeperSoulEssence(Settings settings) {
+        super(settings);
+    }
+
+}
